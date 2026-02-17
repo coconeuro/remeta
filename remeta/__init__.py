df86967779fb8bf29d0ba30997200b112344f706
m-guggenmos <monaden@gmail.com>
2026-02-17 21:56:00 +0100

    Update __init__ in line with recent refactoring

--- remeta/__init__.py
+++ remeta/__init__.py
@@ -1,5 +1,4 @@
 from .model import ReMeta, load_dataset
 from .configuration import Configuration
-from .gendata import simu_data
-from .plot import plot_type1_type2, plot_psychometric, plot_psychometric_sim, plot_evidence_versus_confidence, \
-    plot_confidence_dist, plot_confidence
+from .simulation import simulate
+from .plotting import plot_psychometric, plot_confidence_histogram, plot_stimulus_versus_confidence
