a92ffc2f74212985e7c44c0a6ddd7edc27bccd5f
m-guggenmos <monaden@gmail.com>
2026-03-30 11:27:29 +0200

    Include linearize_stimulus_evidence and check_linearity

--- remeta/__init__.py
+++ remeta/__init__.py
@@ -2,3 +2,4 @@ from .model import ReMeta, load_dataset
 from .configuration import Configuration
 from .simulation import simulate
 from .plotting import plot_psychometric, plot_confidence_histogram, plot_stimulus_versus_confidence
+from .util import linearize_stimulus_evidence, check_linearity
